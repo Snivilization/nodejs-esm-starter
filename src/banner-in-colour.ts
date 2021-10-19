0d65ce8fbc4c85d6479a1b225f8b18b00f9c4744
Plastikfan <plastikfan@outlook.com>
2021-10-19 13:14:28 +0100

    chore: add basic typescript setup (#7)

--- src/banner-in-colour.ts
+++ src/banner-in-colour.ts
@@ -1,12 +1,12 @@
-import fs from 'fs'
-import url from 'url'
+import { promises as fs } from 'fs'
+import url, { URL } from 'url'
 import { join } from 'path'
 import { add } from './add.js'
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
-const text = fs.readFileSync(join(__dirname, 'text.txt'), 'utf8')
+const text = await fs.readFile(join(__dirname, 'text.txt'), 'utf8')
 
-export function bannerInColor(colour) {
+export function bannerInColor(colour: string): string {
   const answer = add(40, 2);
   return `${colour}: ${text}: ${answer}`;
 }
--- test/tryout.cjs
+++
@@ -1,6 +0,0 @@
-const assert = require('assert')
-const { banner } = require('nodejs-esm-starter')
-
-assert.strict.match(banner(), /The answer is.*42/)
-
-console.log(banner())
